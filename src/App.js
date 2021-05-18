ca7aa498b10f9800274482a11054227b20f72c21
Samantha Solomon <sleahsolomon@gmail.com>
2021-05-18 16:23:29 -0400

    partial api call

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ import Endpage from './components/Endpage';
 
 const delay = 5;
 
+const apiLocal = 'http://localhost:4000/api/emotion/'
+
+const apiHeroku = 'https://feelings-api1.herokuapp.com/api/emotion'
+
 export default function App() {
+	
+	//FETCH EMOTION STORY
+	const [emotion, setEmotion] = useState(null)
+
+	const getStory = async = (emotionName) => {
+		const response = await fetch(api + {emotionName},{
+			headers: {Accept: "application/json"}
+		})
+		const data = await response.json()
+		setEmotion(data)
+	}
+	
+	//TIMER DELAY WELCOME PAGE TO LANDING PAGE
 	const [show, setShow] = useState(false);
 
 	useEffect(() => {
