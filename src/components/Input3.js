46197e210583b829db1f8280fdee60519523a190
jjcrab <lily_lijj@hotmail.com>
2021-05-18 11:40:15 -0600

    remove packagelock

---
+++ src/components/Input3.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Input3 = () => {
+	return (
+		<div>
+			<div>
+				<h3>How did that make you feel?</h3>
+				<p>Hopefully not afraid!</p>
+			</div>
+
+			<p>pssssst! You can fill in that box too!</p>
+			<form>
+				<input type='text'></input>
+				<button type='submit'></button>
+			</form>
+		</div>
+	);
+};
+
+export default Input3;
