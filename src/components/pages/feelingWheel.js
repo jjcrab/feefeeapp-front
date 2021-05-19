51d90e71bc091026d32cc2dc97fe9b9678d149c7
ameshalexk <ameshalexusa@gmail.com>
2021-05-19 16:11:07 -0500

    rebased

--- src/components/pages/feelingWheel.js
+++ src/components/pages/feelingWheel.js
@@ -5,52 +5,31 @@ const data = [
   {
     title: 'Anger 😠',
     value: 16,
-    color: '#C4C4C4',
+    color: '#e5222d',
   },
-  { title: 'Fear 😨', value: 16, color: '#C4C4C4' },
-  { title: 'Surprise 😲', value: 16, color: '#C4C4C4' },
-  { title: 'Yuck 🤮', value: 16, color: '#C4C4C4' },
-  { title: 'Joy 😀', value: 16, color: '#C4C4C4' },
-  { title: 'Sadness 😟', value: 16, color: '#C4C4C4' },
+  { title: 'Fear 😨', value: 16, color: '#F49132' },
+  { title: 'Surprise 😲', value: 16, color: '#FBE840' },
+  { title: 'Yuck 🤮', value: 16, color: '#61328F' },
+  { title: 'Happy 😀', value: 16, color: '#4FB74D' },
+  { title: 'Sad 😟', value: 16, color: '#2063AD' },
 ];
 
-function FeelingWheel({ setEmotion }) {
+function FeelingWheel(props) {
   const lineWidth = 85;
-  let emotionWord;
-
-  const getStory = async (emotionId) => {
-    const word = {
-      fear: 1,
-      anger: 2,
-      joy: 3,
-      sadness: 4,
-      yuck: 5,
-      surprise: 6,
-    };
-
-    let apiHeroku = `https://feelings-api1.herokuapp.com/api/emotion/${word[emotionId]}`;
-    const response = await fetch(apiHeroku, {
-      headers: { Accept: 'application/json' },
-    });
-
-    const data = await response.json();
-    // const randomStory = 1 + Math.floor((Math.random() * 2));
-    // console.log(randomStory)
-    // console.log(data, 'sss');
-
-    setEmotion(data);
-  };
+  const [selected, setSelected] = useState(0);
 
   return (
-    <div>
-      <h1 id="container">
+    <div id="container">
+      <h3>
         How are you feeling <br /> today?
-      </h1>
+      </h3>
       <PieChart
         lineWidth={99.9}
-        radius={PieChart.defaultProps.radius - 8}
+        radius={PieChart.defaultProps.radius - 15}
         label={({ dataEntry }) => dataEntry.title}
         data={data}
+        segmentsShift={(index) => (index === selected ? 6 : 1)}
+        animate
         labelStyle={{
           fill: 'black',
           opacity: 1,
@@ -58,12 +37,12 @@ function FeelingWheel({ setEmotion }) {
           fontSize: '5px',
           fontWeight: 'bold',
         }}
-        labelPosition={100 - lineWidth / 2}
-        onClick={(e) => {
+        labelPosition={100 - lineWidth / 2.4}
+        onClick={(_, index) => {
+          setSelected(index === selected ? undefined : index);
           emotionWord = e.target.textContent.toLowerCase().split(' ')[0];
           getStory(emotionWord);
           setEmotion(emotionWord);
-          let nextPage = document.querySelector('carousel-control-next');
         }}
       />
       ;
