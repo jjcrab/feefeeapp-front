948338ae5bbfeda4063275c72956dc04133069bf
Richard Williams <richelliswilliams@gmail.com>
2021-05-18 17:37:28 -0700

    add state for user inputs at App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ const delay = 5;
 
 export default function App() {
 	const [show, setShow] = useState(false);
+  const [userInputs, setUserInputs] = useState(
+    [
+      {text:''},
+      {text:''},
+      {text:''},
+      {text:''},
+      {text:''},
+      {text:''},
+    ]
+  );
+
+  const handleInputChange = (evt) => {
+    let newInput = [...userInputs]
+    newInput[evt.target.name] = {text:evt.target.value}
+    setUserInputs(newInput)
+    console.log(newInput)
+  }
 
 	useEffect(() => {
 		let timer1 = setTimeout(() => setShow(true), delay * 1000);
@@ -17,15 +34,21 @@ export default function App() {
 		};
 	}, []);
 
-	return show ? (
-		<div>
-			<WelcomeScreen />
-		</div>
-	) : (
-		<div>
-			<LandingPage />
-			<Storypage2 />
-			<Endpage />
-		</div>
+
+	return (
+    <div>
+      <input name={0} value={userInputs[0].text} onChange={handleInputChange}></input>
+      <input name={1} value={userInputs[1].text} onChange={handleInputChange}></input>
+      show ? (
+      <div>
+        <WelcomeScreen />
+      </div>
+    ) : (
+      <div>
+        <LandingPage />
+        <Storypage2 />
+        <Endpage />
+      </div>
+    </div>
 	);
 }
