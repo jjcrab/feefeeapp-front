8162886bfd14038177d6b6262030326b8f4dafd9
ameshalexk <ameshalexusa@gmail.com>
2021-05-19 15:58:57 -0500

    rebased

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StoryPage1 from './pages/StoryPage1';
 import StoryPage2 from './pages/StoryPage2';
 import StoryPage3 from './pages/StoryPage3';
@@ -14,48 +14,50 @@ import WelcomeScreen from './pages/welcomeScreen/welcomeScreen';
 import Carousel from 'react-bootstrap/Carousel';
 
 const Slider = ({ userInputs, handleInputChange }) => {
-	return (
-		<div>
-			<Carousel interval={null} className='slider'>
-				<Carousel.Item className='welcomescreen'>
-					<WelcomeScreen />
-				</Carousel.Item>
-				<Carousel.Item>
-					<FeelingWheel />
-				</Carousel.Item>
-				<Carousel.Item>
-					<StoryPage1 />
-				</Carousel.Item>
-				<Carousel.Item>
-					<StoryPage2 />
-				</Carousel.Item>
-				<Carousel.Item>
-					<StoryPage3 />
-				</Carousel.Item>
-				<Carousel.Item>
-					<Userinput1page
-						userInputs={userInputs}
-						handleInputChange={handleInputChange}
-					/>
-				</Carousel.Item>
-				<Carousel.Item>
-					<UserInput2
-						userInputs={userInputs}
-						handleInputChange={handleInputChange}
-					/>
-				</Carousel.Item>
-				<Carousel.Item>
-					<Input3
-						userInputs={userInputs}
-						handleInputChange={handleInputChange}
-					/>
-				</Carousel.Item>
-				<Carousel.Item>
-					<Endpage />
-				</Carousel.Item>
-			</Carousel>
-		</div>
-	);
+  const [emotion, setEmotion] = useState(null);
+
+  return (
+    <div>
+      <Carousel interval={null} className="slider">
+        <Carousel.Item className="welcomescreen">
+          <WelcomeScreen />
+        </Carousel.Item>
+        <Carousel.Item>
+          <StoryPage1 />
+        </Carousel.Item>
+        <Carousel.Item>
+          <StoryPage2 />
+        </Carousel.Item>
+        <Carousel.Item>
+          <StoryPage3 />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Userinput1page
+            userInputs={userInputs}
+            handleInputChange={handleInputChange}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <UserInput2
+            userInputs={userInputs}
+            handleInputChange={handleInputChange}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Input3
+            userInputs={userInputs}
+            handleInputChange={handleInputChange}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Endpage />
+        </Carousel.Item>
+        <Carousel.Item>
+          <FeelingWheel setEmotion={setEmotion} />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Slider;
